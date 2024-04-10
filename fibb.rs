3a0b13e31e95d54600afceefee9f7d1bb6fbe2db
Ethan Chalupa <cha21093@byui.edu>
2024-04-10 12:55:19 -0400

    added comments

--- fibb.rs
+++ fibb.rs
@@ -4,10 +4,10 @@ fn main() {
     println!("Enter the number of Fibonacci numbers to generate:");
     
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    let n: u32 = input.trim().parse().expect("Please enter a valid number");
+    io::stdin().read_line(&mut input).expect("Failed to read line");//Check for invalid inputs
+    let n: u32 = input.trim().parse().expect("Please enter a valid number");// This should stop bad numbers
 
-    let mut fib_sequence: Vec<u64> = vec![0, 1];
+    let mut fib_sequence: Vec<u64> = vec![0, 1];//ugly, but make a stupid array of numbers. Rust is dumb
     for i in 2..n {
         let number = fib_sequence[(i - 1) as usize] + fib_sequence[(i - 2) as usize];
         fib_sequence.push(number);
@@ -19,7 +19,7 @@ fn main() {
     }
 }
 
-fn print_fibonacci(num: u64) {
+fn print_fibonacci(num: u64) { //print out each line in the fibb sequnce
     let width = num as usize;
     let mut line = String::with_capacity(width);
 
